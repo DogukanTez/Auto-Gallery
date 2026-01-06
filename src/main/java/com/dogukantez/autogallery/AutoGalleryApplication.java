c3f9409f9bba0515d0f52a473e233a984b06b4d3
Doğukan Tez <dogukantez7@gmail.com>
2026-01-06 16:28:10 +0300

    project init

---
+++ src/main/java/com/dogukantez/autogallery/AutoGalleryApplication.java
@@ -0,0 +1,13 @@
+package com.dogukantez.autogallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoGalleryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dogukantez/autogallery/AutoGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dogukantez.autogallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
